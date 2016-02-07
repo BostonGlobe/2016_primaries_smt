0881d7e467b4f8dd2ec4f0fa2babb5b30c7c2a7d
Gabriel Florit <gabrielflorit@gmail.com>
2016-02-06 22:01:28 -0500

    Point to devweb

--- src/js/urlManager.js
+++ src/js/urlManager.js
@@ -11,8 +11,9 @@ module.exports = function urlManager(opts) {
 
 	// construct the api url
 	// const baseUrl = 'http://dev.tomcat.boston.com/electionapi/elections/';
+	const baseUrl = 'http://devweb.bostonglobe.com/electionapi/elections/';
 	// const baseUrl = 'http://devweb.bostonglobe.com/electionapi/elections/';
-	const baseUrl = 'https://www.bostonglobe.com/electionapi/elections/';
+	// const baseUrl = 'https://www.bostonglobe.com/electionapi/elections/';
 	const url     = `${baseUrl}${date}?statePostal=${stateAbbr}&party=${partyAbbr}&level=${level}`;
 
 	return url;
